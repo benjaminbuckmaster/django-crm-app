19e36d822ea2cd5d7c32e437ad4c5286bac8737a
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-08 14:35:56 +1100

    added log in page

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
+
+    
     return render(request, 'home.html', context={})
+
+def logout_user(request):
+    pass
