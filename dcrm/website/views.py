2b6ce598adb253308c6cf5a3e4543ef8fc2cab3b
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-08 14:55:59 +1100

    completed log in page

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
+    # check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-    
-    return render(request, 'home.html', context={})
+        # authenticate the user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Log in successful")
+            return redirect('home')
+        else:
+            messages.success(request, "Login error. Plase try again.")
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html', context={})
 
 def logout_user(request):
     pass
