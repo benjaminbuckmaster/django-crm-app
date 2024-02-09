5d4eee07bdcb3389ece59fd03488c9648031b6be
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-09 15:34:32 +1100

    added ability to delete record

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.logout_user, name='logout'),
     path('record/<int:pk>', views.customer_record, name='record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
 ]
